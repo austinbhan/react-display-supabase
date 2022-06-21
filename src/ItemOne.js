28fae8d0ea7d2e1441639d42ab6d77978c19e669
austinbhan <austinbhan@outlook.com>
2022-06-21 12:07:43 -0700

    .env file

--- src/ItemOne.js
+++ src/ItemOne.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+export default function fruit({ name, color, price }) {
+  return (
+    <div className="fruit">
+      <p>{name}</p>
+      <p>{color}</p>
+      <p>{price}</p>
+    </div>
+  );
+}
