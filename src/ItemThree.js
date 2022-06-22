19ee986a1898685b1243bd6d99eba94f720a4e17
austinbhan <austinbhan@outlook.com>
2022-06-22 14:15:49 -0700

    VegetableList

--- src/ItemThree.js
+++ src/ItemThree.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+export default function Vegetable({ name, color, quantity }) {
+  return (
+    <div className="vegetable">
+      <p>{name}</p>
+      <p>{color}</p>
+      <p>{quantity}</p>
+    </div>
+  );
+}
--- src/ListThree.js
+++ src/ListThree.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Vegetable from './ItemThree';
 
+export default function VegetableList({
+  vegetables = [],
+}) {
+  return (
+    <div className='vegetable-list'>
+      {
+        vegetables.map((vegetable, i) => {
+          return <Vegetable 
+            {...vegetable} 
+            key={vegetable.name + i + vegetable.id} />;
+        })
+      }
+    </div>
+  );
+}
