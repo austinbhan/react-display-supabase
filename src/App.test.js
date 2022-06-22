ff6fe8c532d6ca7b280165485f90449c66724d8a
austinbhan <austinbhan@outlook.com>
2022-06-22 15:06:53 -0700

    Sebastion test

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,8 @@
-test('fake test', (expect) => expect(true).toBe(true));
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+test('renders learn react link', () => {
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
